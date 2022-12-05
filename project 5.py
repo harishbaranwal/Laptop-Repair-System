3f8d53bb310bed89d2ee04cd82d9d1a2a772a74a
harishbaranwal <114431658+harishbaranwal@users.noreply.github.com>
2022-12-05 19:38:42 +0530

    Update project 5.py

--- project 5.py
+++ project 5.py
@@ -1,28 +1,39 @@
-# Guess the number into 3 chances
-import math
-import random
-# Enter the lower range
-lower_range =int(input("Enter lower range : "))
-# Enter the upper range
-upper_range = int(input("Enter upper range : "))
-random_num=random.randint(lower_range,upper_range)
-print("You have only 3 chance to guess the integer")
-i=0
-while i<3:
-    i+=1
-# User Guess the number into given range    
-    user_num=int(input("Guess a number : "))
-    if random_num==user_num:
-        print("Congrat's")
-        print("The number is",random_num)
-        print("You done it in",i,"try")
-        break
-    elif random_num>user_num:
-        print("Your guess was too small") 
-        print("Have one more try")
-    elif random_num<user_num:
-        print("Your guess was too high")
-        print("Have one more try")
-if i>=3:
-    print("The number is",random_num) 
-    print("Better Luck Next Time!")              
+# Guess the number
+try:
+# try and except is used for valid range     
+    from random import randint
+    level = 1
+    points = [30,20,10]
+    points_scored = 0
+    # use while loop for increasing level upto 1000000
+    while level <= 100000:
+        a=int(input("Enter the lower bound of the range: "))
+        b=int(input("Enter the upper bound of the range: "))
+
+        secret_number = randint(a,b)
+        counter=0
+    # use while loop for repetitive guessing     
+        while counter<3:
+            guess_number = int(input("Guess the secret number between {} and {} ({} attempts left): ".format(a,b,3-counter)))
+
+            if secret_number == guess_number:
+                print("Congrats, You Won the level {} with {} points".format(level,points[counter]))
+                points_scored += points[counter]
+                level = level + 1
+                break
+            elif guess_number < secret_number:
+                print("Your guess was too low.")
+            else:
+                print("Your guess was too high.")
+            counter+=1    
+        else:
+            print("Better Luck Next Time!, secret number was {}.".format(secret_number))
+            print("Your total score is {} points.".format(points_scored))
+            break
+    else:
+        print("Congrats, You Won the Game with {} points!!!".format(points_scored))
+
+except:
+    print("Error! Enter proper values for the range.")
+ # This is the end of the program
+               
